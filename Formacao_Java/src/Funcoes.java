98fba196140720f1d960aacaea17647367cdf51d
usuario <usuario@MACHINE-NOT>
2019-03-31 22:46:10 -0300

    atualizando funcao

--- Formacao_Java/src/Funcoes.java
+++ Formacao_Java/src/Funcoes.java
@@ -1,41 +1,46 @@
 import java.util.Scanner;
 
 public class Funcoes {
+	
+	
+	public void cadastrarFrutas() {
+		//Funçoes
+				int qtd = 0;
+				String frutas[];
+				
+				System.out.println("Informe a quantidade de frutas para cadastrar:");
+				
+				Scanner teclado = new Scanner(System.in);
+				qtd = Integer.parseInt(teclado.nextLine());
+				
+				frutas = new String[qtd];
+				
+				if (qtd > 0) {
+					
+					for (int i = 0; i < qtd; i++) {
+						System.out.println("Informe a fruta "+(i + 1));
+						frutas[i] = teclado.nextLine();	
+					}
+					
+					System.out.println(qtd);
+					for (int i = qtd; i > 0; i--) {
+						System.out.println(frutas[i - 1]);
+					}
+					
+				}
+				
+				
+				
+				
+				
+				
+				teclado.close();
+
+
+	}
 
 	public static void main(String[] args) {
 		
-		//Funçoes
-		int qtd = 0;
-		String frutas[];
-		
-		System.out.println("Informe a quantidade de frutas para cadastrar:");
-		
-		Scanner teclado = new Scanner(System.in);
-		qtd = Integer.parseInt(teclado.nextLine());
-		
-		frutas = new String[qtd];
-		
-		if (qtd > 0) {
-			
-			for (int i = 0; i < qtd; i++) {
-				System.out.println("Informe a fruta "+(i + 1));
-				frutas[i] = teclado.nextLine();	
 			}
-			
-			System.out.println(qtd);
-			for (int i = qtd; i > 0; i--) {
-				System.out.println(frutas[i - 1]);
-			}
-			
-		}
-		
-		
-		
-		
-		
-		
-		teclado.close();
-
-	}
 
 }
